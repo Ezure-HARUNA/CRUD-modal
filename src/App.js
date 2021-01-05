eb1be25cf9f40144c64492b2b9200de7a25b08c5
ezure haruna <burimofumofu@gmail.com>
2021-01-05 15:07:55 +0900

    #1:feat delete・update機能の追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 
 const App = () => {
   const usersData = [
@@ -12,6 +13,7 @@ const App = () => {
   const [users, setUsers] = useState(usersData)
   const [editing, setEditing] = useState(false)
   const initialFormState = { id: null, name: '', username: '' }
+  const [currentUser, setCurrentUser] = useState(initialFormState)
 
   const addUser = (user) => {
     user.id = users.length + 1
@@ -28,13 +30,32 @@ const App = () => {
     setCurrentUser({ id: user.id, name: user.name, username: user.username })
   }
 
+  const updateUser = (id, updatedUser) => {
+    setEditing(false)
+  
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)))
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser} />
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
