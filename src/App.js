b4e148e770be8b648cc9f583e163ea75de30dca3
ezure haruna <burimofumofu@gmail.com>
2021-01-05 14:58:32 +0900

    #1:feat Form componentの追加

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
+  const initialFormState = { id: null, name: '', username: '' }
+
+  const addUser = (user) => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id))
+  }
+
+  const editRow = (user) => {
+    setEditing(true)
+  
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+          <UserTable users={users} 
+            editRow={editRow} deleteUser={deleteUser} 
+          />
         </div>
       </div>
     </div>
